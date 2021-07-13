469b8e362554ff142cd58e7b43e598d8100e15bc
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-07-13 18:37:13 +0100

    first commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Button from "@material-ui/core/Button"
-import vectorone from "../svg/vector1.svg"
+// import vectorone from "../svg/vector1.svg"
 import money from '../svg/money.svg'
 import '../styles/banner.css'
 
